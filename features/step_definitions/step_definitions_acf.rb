a690a929eb43e8f9c07fe35e9fa5c8b3cdb0ae4b
Ankur Goel <ankurgel@gmail.com>
2013-07-23 03:09:13 +0530

    Added cucumber features

---
+++ features/step_definitions/step_definitions_acf.rb
@@ -0,0 +1,8 @@
+require 'statsample'
+include Statsample::TimeSeries
+
+#all instance variable and cucumber DSL s DRYed up in step_definitions.rb
+And /^I calculate acf$/ do
+  @result = @timeseries.acf(@lags)
+end
+
