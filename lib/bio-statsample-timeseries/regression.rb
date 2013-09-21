3e0ecd88190b04286eeb3dc42c8966aa049fba26
Ankur Goel <ankurgel@gmail.com>
2013-09-21 19:58:55 +0530

    Coded IRWLS. Have to crosscheck the evaluation etc.

--- lib/bio-statsample-timeseries/regression.rb
+++ lib/bio-statsample-timeseries/regression.rb
@@ -11,10 +11,24 @@ module Statsample
 
     def self.irwls(x, y, mu, w, j, h, epsilon = 1e-7, max_iter = 100)
       b = Matrix.column_vector(Array.new(x.column_size,0.0))
-      converted = false
+      converged = false
       1.upto(max_iter) do |i|
-        b_new #continuing
+        #conversion from : (solve(j(x,b)) %*% h(x,b,y))
+        intermediate = (j.call(x,b) * h.call(x,b,y))
+        b_new = b - intermediate
+        if((b_new - b).map(&:abs)).to_a.flatten.inject(:+) < epsilon
+          converged = true
+          b = b_new
+          break
+        end
+        b = b_new
       end
+
+      ss = j.call(x,b).inverse.diagonal.map{ |x| - x}.map{ |y| Math.sqrt(y) }
+      values = mu.call(x,b)
+      residuals = y - values
+      df_residuals = y.count - x.column_size
+      return [b, ss, mu.call(x,b), residuals, max_iter, df_residuals, converged]
     end
 
   end
