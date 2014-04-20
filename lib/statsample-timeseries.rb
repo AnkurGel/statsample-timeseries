4c3dbf0c143813b3e437cf69e262ebfcb31168a3
Carlos Agarie <carlos.agarie@gmail.com>
2014-04-20 12:45:32 -0300

    Removed BioRuby-related comment
    This comment is unnecessary as this is a general scientific gem.

--- lib/statsample-timeseries.rb
+++ lib/statsample-timeseries.rb
@@ -1,13 +1,3 @@
-# Please require your code below, respecting the naming conventions in the
-# bioruby directory tree.
-#
-# For example, say you have a plugin named bio-plugin, the only uncommented
-# line in this file would be
-#
-#   require 'bio/bio-plugin/plugin'
-#
-# In this file only require other files. Avoid other source code.
-
 require 'statsample'
 require 'statsample-timeseries/version'
 
